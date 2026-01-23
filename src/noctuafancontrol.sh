b1fb763613e62363fb1e56d966267efb11be4307
Halhadus <eneshakans45@proton.me>
2026-01-23 21:13:59 +0300

    Fan script update

--- src/noctuafancontrol.sh
+++ src/noctuafancontrol.sh
@@ -4,12 +4,11 @@ PWM_CHIP="/sys/class/pwm/pwmchip0"
 PWM_ID="0"
 PWM_PATH="$PWM_CHIP/pwm$PWM_ID"
 
-POLL_INTERVAL=1.5
+PERIOD=40000
+POLL_INTERVAL=2
 RAMP_STEP_DELAY=0.02
 RAMP_PERCENT_PER_STEP=2
-HYSTERESIS=3
-
-PERIOD=40000
+HYSTERESIS=2
 
 find_cpu_sensor() {
     for zone in /sys/class/thermal/thermal_zone*; do
@@ -49,79 +48,67 @@ percent_to_duty() {
     echo $(( (percent * PERIOD) / 100 ))
 }
 
-current_duty=$(cat "$PWM_PATH/duty_cycle" 2>/dev/null || echo 0)
+get_target_speed_from_temp() {
+    local t=$1
+    if [ $t -lt 40 ]; then echo 0
+    elif [ $t -lt 45 ]; then echo 30
+    elif [ $t -lt 50 ]; then echo 40
+    elif [ $t -lt 55 ]; then echo 50
+    elif [ $t -lt 60 ]; then echo 60
+    elif [ $t -lt 65 ]; then echo 75
+    elif [ $t -lt 70 ]; then echo 85
+    else echo 100
+    fi
+}
 
 apply_speed() {
     local target_percent=$1
     local target_duty=$(percent_to_duty $target_percent)
-    
-    if [ $target_duty -eq $current_duty ]; then return; fi
-
+    local current_duty_raw=$(cat "$PWM_PATH/duty_cycle" 2>/dev/null || echo 0)
+    if [ "$target_duty" -eq "$current_duty_raw" ]; then return; fi
     local step=$(percent_to_duty $RAMP_PERCENT_PER_STEP)
     if [ "$step" -eq 0 ]; then step=100; fi
-
-    while [ $current_duty -ne $target_duty ]; do
-        if [ $current_duty -lt $target_duty ]; then
-            current_duty=$((current_duty + step))
-            if [ $current_duty -gt $target_duty ]; then current_duty=$target_duty; fi
+    local temp_duty=$current_duty_raw
+    while [ $temp_duty -ne $target_duty ]; do
+        if [ $temp_duty -lt $target_duty ]; then
+            temp_duty=$((temp_duty + step))
+            if [ $temp_duty -gt $target_duty ]; then temp_duty=$target_duty; fi
         else
-            current_duty=$((current_duty - step))
-            if [ $current_duty -lt $target_duty ]; then current_duty=$target_duty; fi
+            temp_duty=$((temp_duty - step))
+            if [ $temp_duty -lt $target_duty ]; then temp_duty=$target_duty; fi
         fi
-        
-        echo $current_duty > "$PWM_PATH/duty_cycle"
+        echo $temp_duty > "$PWM_PATH/duty_cycle"
         sleep $RAMP_STEP_DELAY
     done
 }
 
 cleanup() {
-    local safe_duty=$(percent_to_duty 100)
-    echo $safe_duty > "$PWM_PATH/duty_cycle"
+    echo $(percent_to_duty 100) > "$PWM_PATH/duty_cycle"
     exit 0
 }
 
 trap cleanup EXIT SIGTERM SIGINT
 
 setup_pwm
+LAST_TARGET=0
 
-LAST_APPLIED_TARGET=0
-LAST_TEMP_AT_CHANGE=0
+echo "Fan control started on: $THERMAL_FILE"
 
 while true; do
     TEMP=$(get_cpu_temp)
-    
-    if [ $TEMP -lt 40 ]; then
-        CALC_TARGET=0
-    elif [ $TEMP -lt 45 ]; then
-        CALC_TARGET=30
-    elif [ $TEMP -lt 50 ]; then
-        CALC_TARGET=40
-    elif [ $TEMP -lt 55 ]; then
-        CALC_TARGET=50
-    elif [ $TEMP -lt 60 ]; then
-        CALC_TARGET=60
-    elif [ $TEMP -lt 65 ]; then
-        CALC_TARGET=75
-    elif [ $TEMP -lt 70 ]; then
-        CALC_TARGET=85
-    else
-        CALC_TARGET=100
-    fi
-
-    FINAL_TARGET=$CALC_TARGET
-
-    if [ $CALC_TARGET -lt $LAST_APPLIED_TARGET ]; then
-        if [ $TEMP -gt $((LAST_TEMP_AT_CHANGE - HYSTERESIS)) ]; then
-            FINAL_TARGET=$LAST_APPLIED_TARGET
+    RAW_TARGET=$(get_target_speed_from_temp $TEMP)
+    if [ "$RAW_TARGET" -lt "$LAST_TARGET" ]; then
+        HYST_TEMP=$((TEMP + HYSTERESIS))
+        HYST_TARGET=$(get_target_speed_from_temp $HYST_TEMP)
+        if [ "$HYST_TARGET" -lt "$LAST_TARGET" ]; then
+             FINAL_TARGET=$RAW_TARGET
+        else
+             FINAL_TARGET=$LAST_TARGET
         fi
+    else
+        FINAL_TARGET=$RAW_TARGET
     fi
-
     apply_speed $FINAL_TARGET
-
-    if [ $FINAL_TARGET -ne $LAST_APPLIED_TARGET ]; then
-        LAST_APPLIED_TARGET=$FINAL_TARGET
-        LAST_TEMP_AT_CHANGE=$TEMP
-    fi
-
+    LAST_TARGET=$FINAL_TARGET
     sleep $POLL_INTERVAL
 done
